ffca5c6bff80e0ce67fff2da2712b03dc23acb6b
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2025-03-16 03:50:49 +0530

    Added Login logout and Navbar in the client

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,11 +4,16 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
