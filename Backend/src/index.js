ed15fdcd30ab3a296136eac59e3012c48710b2f3
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2025-03-18 05:15:28 +0530

    Final Commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -8,6 +8,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
 
 dotenv.config();
 // const app = express();
@@ -21,6 +22,10 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cookieParser());
+
+const port= process.env.PORT;
+const __dirname = path.resolve();
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
@@ -29,7 +34,13 @@ app.use(cors({
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
-const port= process.env.PORT;
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+      });
+}
 
 server.listen(port, ()=>{
     console.log(`server is running on port ${port}`);
